f3d159b3f5c567f46dc22eb99c03a411d0f8c6e4
Varun <vsrin00@gmail.com>
2026-07-16 17:29:25 -0400

    Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://VarunS00.github.io",
+  base: "/devportfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
